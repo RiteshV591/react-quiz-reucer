c008d920413d3f983e6cfb3376bfd82190e72e82
Ritesh Vaghela <riteshv@webelight.co.in>
2024-02-27 19:02:24 +0530

    questions loading from fake API

---
+++ src/Components/Header.tsx
@@ -0,0 +1,8 @@
+export const Header = () => {
+  return (
+    <header className="w-[66rem] mb-16 flex items-center justify-between">
+      <img className="w-[14rem]" src="logo512.png" alt="React logo" />
+      <h1 className="font-[Codystar] text-[5.6rem]">The React Quiz</h1>
+    </header>
+  );
+};
